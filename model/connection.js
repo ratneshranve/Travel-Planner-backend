8d20111ed5194af4ae4a51f5ff50feda0449d1b7
ratneshranve <ratneshranve@gmail.com>
2025-07-19 11:26:00 +0530

    connection

--- model/connection.js
+++ model/connection.js
@@ -3,11 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load .env variables
 
-const url = process.env.MONGODB_URI;
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("✅ MongoDB Atlas connected"))
 .catch(err => console.error("❌ MongoDB connection failed:", err));
